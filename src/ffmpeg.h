7ca46d417323f9e91d10154bcb003c2e285939f3
YS-RAPTOR <yashan.sumanaratne@gmail.com>
2025-04-27 15:06:18 +1000

    Forgot to add c libraries

---
+++ src/ffmpeg.h
@@ -0,0 +1,7 @@
+#define __STDC_CONSTANT_MACROS
+#include <libavcodec/avcodec.h>
+#include <libavfilter/avfilter.h>
+#include <libavfilter/buffersink.h>
+#include <libavfilter/buffersrc.h>
+#include <libavutil/imgutils.h>
+#include <libavutil/opt.h>

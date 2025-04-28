686c954fa316a02ac95874a5bbd1270973c52631
YS-RAPTOR <yashan.sumanaratne@gmail.com>
2025-04-28 23:53:59 +1000

    Updated receiver to output mp4 and added text to get comparison. Also updated the text displayed.

--- Tester/src/receiver.bat
+++ Tester/src/receiver.bat
@@ -1,25 +1,38 @@
 @echo off
-REM Usage: receiver.bat [udp|rtp|srt]
+REM Usage: receiver.bat [udp|rtp|srt|rist] output.mp4
 
 set PROTO=%1
+set OUTPUT=%2
 set PORT=2004
 
 if "%PROTO%"=="" (
-    echo Usage: %~nx0 [udp^|rtp^|srt^|rist]
+    echo Usage: %~nx0 [udp^|rtp^|srt^|rist] output.mp4
     exit /b 1
 )
 
+if "%OUTPUT%"=="" (
+    echo You must specify an output filename [e.g. output.mp4]
+    exit /b 1
+)
+
+REM Text overlay settings (match sender)
+set FONT="C\\:/Windows/Fonts/arial.ttf"
+set TEXT="Time\\: %%{localtime\\:%%X.%%N} (%%{pts\\:hms}) Frame\\: %%{n}"
+set DRAW=drawtext=fontfile=%FONT%:text=%TEXT%:fontsize=48:fontcolor=white:x=10:y=100:box=1:boxcolor=black
+set DURATION=300
+
+REM Choose ffmpeg input and apply drawtext filter; re-encode video to allow filtering
 if /i "%PROTO%"=="udp" (
-    ffplay "udp://127.0.0.1:%PORT%"
+    ffmpeg -y -i "udp://127.0.0.1:%PORT%" -t %DURATION% -vf %DRAW% -c:v h264_nvenc -preset fast -cq 23 -c:a copy "%OUTPUT%"
     exit /b
 ) else if /i "%PROTO%"=="rtp" (
-    ffplay -protocol_whitelist file,udp,rtp -i stream.sdp
+    ffmpeg -y -protocol_whitelist file,udp,rtp -i stream.sdp -t %DURATION% -vf %DRAW% -c:v h264_nvenc -preset fast -cq 23 -c:a copy "%OUTPUT%"
     exit /b
 ) else if /i "%PROTO%"=="srt" (
-    ffplay "srt://127.0.0.1:%PORT%?mode=listener"
+    ffmpeg -y -i "srt://127.0.0.1:%PORT%?mode=listener" -t %DURATION% -vf %DRAW% -c:v h264_nvenc -preset fast -cq 23 -c:a copy "%OUTPUT%"
     exit /b
 ) else if /i "%PROTO%"=="rist" (
-    ffplay "rist://@:%PORT%"
+    ffmpeg -y -i "rist://@:%PORT%" -t %DURATION% -vf %DRAW% -c:v h264_nvenc -preset fast -cq 23 -c:a copy "%OUTPUT%"
     exit /b
 ) else (
     echo Unsupported protocol: %PROTO%
--- Tester/src/sender.bat
+++ Tester/src/sender.bat
@@ -12,7 +12,7 @@ if "%PROTO%"=="" (
 
 REM Set input options
 set FONT="C\\:/Windows/Fonts/arial.ttf"
-set TEXT="Time\\: %%{localtime} (%%{pts\\:hms}) Frame\\: %%{n}"
+set TEXT="Time\\: %%{localtime\\:%%X.%%N} (%%{pts\\:hms}) Frame\\: %%{n}"
 set INPUT=-re -f lavfi -i testsrc=rate=60:size=1920x1080 -vf drawtext=fontfile=%FONT%:text=%TEXT%:fontsize=48:fontcolor=white:x=10:y=10:box=1:boxcolor=black
 
 if /i "%PROTO%"=="udp" (
